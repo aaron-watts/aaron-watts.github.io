f83d0d4e2aec76cc9721ae881cccafc09863b534
aaron-watts <retrolasered@gmail.com>
2021-09-16 18:55:21 +0100

    add box shadow to projects section content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
-const navLinks = document.querySelectorAll('nav.main a, .links a');
+// for adjusting nav color to fit backgorund
 const navBar = document.querySelector('nav.main');
+
+// for CLI affect on hero section
+const navLinks = document.querySelectorAll('nav.main a.type, .links a i');
 const command = document.querySelector('#command');
 
+// for carousel on Projects section
 const imgs = [...document.querySelectorAll('img.thumb')];
 const carouselLeft = document.querySelector('.carousel-left');
 const carouselRight = document.querySelector('.carousel-right');
 
-const commands = ['cd ~', 'whoami', 'ls ~/Documents/', 'ping 127.0.0.1', 'git remote -v'];
+// colors to adjust nav color
 const primaryNavColor = '#2d302f'
 const secondaryNavColor = '#373838';
 
@@ -21,11 +25,12 @@ window.addEventListener('scroll', () => {
     else navBar.style.backgroundColor = secondaryNavColor;
 })
 
-// type one letter at a time with delay to create a typing animation
+// Typing animation function
+// Type one letter at a time with delay to create a typing effect
 const typeText = async (text) => {
     if (text.length > 0) {
         command.innerText += text[0];
-        // assign setTimeout to global variable to clear if no longer hovering over link
+        // assign setTimeout to global to allow animation to be cancelled when no longer hovering
         typeInterval = setTimeout(function () { typeText(text.substring(1, text.length)) }, 60);
     }
     else return;
@@ -33,11 +38,12 @@ const typeText = async (text) => {
 
 for (nav of navLinks) {
     // type relevant text to command line on hover
+    // command is assigned to html in customText attribute
     nav.addEventListener('mouseover', (evt) => {
-        typeText(commands[parseInt(evt.target.id.replace('link-', ''))]);
+        typeText(evt.target.attributes.customText.value);
     })
 
-    // clear text and interval if no hover
+    // clear interval and text if no hover
     nav.addEventListener('mouseout', (evt) => {
         clearTimeout(typeInterval);
         command.innerText = '';
@@ -46,7 +52,9 @@ for (nav of navLinks) {
 
 // Carousel function
 // active refers to index of active image in imgs array
+// direction is positive or negative depending on left or right buttons
 const turnCarousel = (active, direction) => {
+    // Get directions in text form for class assignments
     const dir = direction > 0 ? 'right' : 'left';
     const opp = dir === 'right' ? 'left' : 'right';
 
@@ -60,6 +68,7 @@ const turnCarousel = (active, direction) => {
 carouselLeft.addEventListener('click', () => {
     // find index of image with class of active
     const active = imgs.findIndex(img => img.classList.contains('active'));
+
     // check we are not at the edge of the carousel
     if (active !== imgs.length - 1) turnCarousel(active, 1);
 })
@@ -67,6 +76,7 @@ carouselLeft.addEventListener('click', () => {
 carouselRight.addEventListener('click', () => {
     // find index of image with class of active
     const active = imgs.findIndex(img => img.classList.contains('active'));
+
     // check we are not at the edge of the carousel
     if (active > 0) turnCarousel(active, -1);
 })

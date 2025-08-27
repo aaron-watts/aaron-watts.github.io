d450fbef1c9f3f022c805d1e774f6b32e33a8c07
Aaron Watts <aaron.watts@murena.io>
2025-08-27 15:55:46 +0100

    rewrite test functions to return test result

--- test/tests.py
+++ test/tests.py
@@ -10,168 +10,215 @@ def comments_has_id(html_doc):
     """
     commentSectionID is present in script tag
     """
+    result = {}
     script = html_doc["soup"].select_one("body > script").string
-    result = re.search(r'commentSectionId: "\S+"', script)
-    if result is None:
-        print(f"TEST FAILED: No comment section ID in {html_doc['path']}")
-        return False
-    print("TEST PASSED: comment section ID present")
-    return False
+    test = re.search(r'commentSectionId: "\S+"', script)
+    if test is None:
+        result["msg"] = "FAILED: Comment config has ID"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: Comment config has ID"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def keywords_not_blank(html_doc):
     """
     meta keywords is not blank
     """
+    result = {}
     keywords = html_doc["soup"].select_one("meta[name='keywords']")["content"]
     if len(keywords) > 0:
-        print("TEST PASSED: meta keywords is not blank")
-        return True
-    print(f"TEST FAILED: meta keywords is blank in {html_doc['path']}")
-    return False
+        result["msg"] = "PASSED: meta keywords is not blank"
+        result["passed"] = True
+    else:
+        result["msg"] = "FAILED: meta keywords is blank" 
+        result["passed"] = False
+    print(result["msg"])
+    return result
 
 def description_not_blank(html_doc):
     """
     meta description is not blank
     """
+    result = {}
     desc = html_doc["soup"].select_one("meta[name='description']")["content"]
     if len(desc) > 0:
-        print("TEST PASSED: meta description is not blank")
-        return True
-    print(f"TEST FAILED: meta description is blank in {html_doc['path']}")
-    return False
+        result["msg"] = "PASSED: meta description is not blank"
+        result["passed"] = True
+    else:
+        result["msg"] = "FAILED: meta description is blank"
+        result["passed"] = False
+    print(result["msg"])
+    return result
 
 def breadcrumb_matches_path(html_doc):
     """
     nav breadcrumb matches path
     """
+    result = {}
     breadcrumb = html_doc["soup"].select_one("nav.breadcrumbs").text
     breadcrumb_trail = re.sub(r"aaronwatts@dev:| \$", '', breadcrumb)
     breadcrumb_path = "docs" + breadcrumb_trail.strip() +".html"
     if not breadcrumb_path == html_doc['path']:
-        print(f"TEST FAILED: breadcrumb does not match path in {html_doc['path']}")
-        return False
-    print("TEST PASSED: breadcrumb matches path")
-    return True
-
+        result["msg"] = "FAILED: breadcrumb does not match path"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: Breadcrumb matches path"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def intro_is_included(html_doc):
     """
     First p element has id of intro
     """
+    result = {}
     intro = html_doc["soup"].select_one("main > p:first-of-type")
     if not intro["id"] == "intro":
-        print(f"TEST FAILED: intro does not have id of intro in {html_doc['path']}")
-        return False
-    print("TEST PASSED: intro has id of intro")
-    return True
+        result["msg"] = "FAILED: Intro does not have id of intro"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: Intro has id of intro"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def time_innerhtml_formatted(html_doc):
     """
     time element innerHTML conforms to D MMM, yyyy
     """
+    result = {}
     time_text = html_doc["soup"].select_one("time").text
-    result = re.search(r"\d{1,2}\w{2}\s\w{3,8},\s\d{4}", time_text)
-    if result is None:
-        print(f"TEST FAILED: date inneirText formatted incorrectly in {html_doc['path']}")
-        return False
-    print("TEST PASSED: date innerText foratted correctly")
-    return True
+    test = re.search(r"\d{1,2}\w{2}\s\w{3,8},\s\d{4}", time_text)
+    if test is None:
+        result["msg"] = "FAILED: date innerText formatted incorrectly"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: date innerText foratted correctly"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def time_datetime_formatted(html_doc):
     """
     time elements datetime attribute is correctly formatted
     """
+    result = {}
     datetime = html_doc["soup"].select_one("time")["datetime"]
-    result = re.search(r"\d{4}-\d{2}-\d{2}", datetime)
-    if result is None:
-        error_msg = "TEST FAILED: time element datetime attr"
-        error_msg += " does not conform to yyyy-mm-dd in "
-        error_msg += html_doc["path"]
-        print(error_msg)
-        return False
-    print("TEST PASSED: time elements datetime attribute conforms to yyyy-mm-dd")
-    return True
+    test = re.search(r"\d{4}-\d{2}-\d{2}", datetime)
+    if test is None:
+        result["msg"] = "FAILED: datetime attribute not yyyy-mm-dd"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: datetime attribute is yyyy-mm-dd"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def h1_has_text(html_doc):
     """
     h1 contains text
     """
+    result = {}
     header1 = html_doc["soup"].select_one("main > h1")
     if len(header1.text) == 0:
-        print(f"TEST FAILED: h1 is blank in {html_doc['path']}")
-        return False
-    print("TEST PASSED: h1 has text")
-    return True
+        result["msg"] = "FAILED: h1 is blank"
+        result["passsed"] = False
+    else:
+        result["msg"] = "PASSED: H1 is not blank"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def nav_anchors_have_targets(html_doc):
     """
     nav anchors have corresponding targets
     """
-    passed = True
+    result = {}
+    test = True
     if html_doc["parent_dir"] == "tech":
-        print("TEST NOT REQUIRED: No page nav for tech articles")
-        return True
+        result["msg"] = "OMITTED: tech pages do not use nav"
+        result["passed"] = True
+        print(result["msg"])
+        return result
     nav_links = html_doc["soup"].select("nav li > a")
     for nav_link in nav_links:
         href = nav_link["href"]
         targets = html_doc["soup"].select(href)
         target_n = len(targets)
-        if target_n == 0:
-            print(f"TEST FAILED: Too many targets for href ${href} in ${html_doc['path']}")
-            passed = False
-        if target_n > 1:
-            print(f"TEST FAILED: No target for href ${href} in ${html_doc['path']}")
-            passed = False
-    if not passed:
-        return False
-    print("TEST PASSED: hrefs for page headers all have targets")
-    return True
+        if target_n == 0 or target_n > 1:
+            test = False
+    if test:
+        result["msg"] = "PASSED: Nav links have corresponding headings"
+        result["passed"] = True
+    else:
+        result["msg"] = "FAILED: Nav links do not have corresponding headings"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def img_exists(html_doc):
     """
     img src exists in images folder
     """
+    result = {}
     img_src = html_doc["soup"].select_one("main > img")["src"]
     if os.path.exists(f"docs/{img_src}"):
-        print("TEST PASSED: img src exists in docs/images")
-        return True
-    print(f"FAILED TEST: img src does not exist in docs/images for {html_doc['path']}")
-    return False
+        result["msg"] = "PASSED: img src exists in docs/images"
+        result["passed"] = True
+    else:
+        result["msg"] = "FAILED: Img src does not exist in docs/images"
+        result["passed"] = False
+    print(result["msg"])
+    return result
 
 def main_img_alt(html_doc):
     """
     Article image has alt text
     """
+    result = {}
     img = html_doc["soup"].select_one("main > img")
     alt = img["alt"]
     if len(alt) == 0:
-        print(f"FAILED TEST: img has no alt text in {html_doc['path']}")
-        return False
-    print("TEST PASSED: img has alt text")
-    return True
+        result["msg"] = "FAILED: img has no alt text"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: img has alt text"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def main_img_rsc(html_doc):
     """
     Article images has href
     """
+    result = {}
     img = html_doc["soup"].select_one("main > img")
     src = img["src"]
     if len(src) == 0:
-        print(f"FAILED TEST: img has no src in {html_doc['path']}")
-        return False
-    print("TEST PASSED: img has src")
-    return True
+        result["msg"] = "FAILED: img has no src"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: img has src"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 def no_comments(html_doc):
     """
     HTML doc contains no comments
     """
+    result = {}
     comments = html_doc["soup"].find_all(string=lambda text: isinstance(text, Comment))
     if len(comments) > 0:
-        print(f"FAILED TEST: comments in doc in {html_doc['path']}")
-        return False
-    print("TEST PASSED: No comments in doc")
-    return True
+        result["msg"] = "FAILED: comments in doc"
+        result["passed"] = False
+    else:
+        result["msg"] = "PASSED: no comments in doc"
+        result["passed"] = True
+    print(result["msg"])
+    return result
 
 tests = [
         no_comments,
